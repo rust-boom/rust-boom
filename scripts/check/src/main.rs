dad2f63a1df50a6916e96ffc794696058c348681
Lydanne <1829913225@qq.com>
2024-08-18 16:26:33 +0800

    feat: 支持生成新目录

--- scripts/check/src/main.rs
+++ scripts/check/src/main.rs
@@ -1,31 +1,68 @@
-use std::fs;
+use std::{fs, vec};
 
 fn main() {
-    let readme = fs::read("./readme.md").unwrap();
+    let readme_content = fs::read("./readme.md").unwrap();
 
-    let binding = String::from_utf8(readme).unwrap();
-    let readme = binding.split("\n");
+    let readme_content = String::from_utf8(readme_content).unwrap();
+    let readme = readme_content
+        .split("\n")
+        .enumerate()
+        .map(|(index, line)| (index, line))
+        .collect::<Vec<(usize, &str)>>();
+    let mut res = vec!["| 导航 | 徽章 |".to_string(), "| :--- | :--- |".to_string()];
+    let mut t = [String::new(), String::new(), String::new(), String::new()];
 
-    for line in readme {
-        let reg_link = regex::Regex::new(r"\[(.*)\]\((.*)\)").unwrap();
-
-        let link = reg_link.captures(line);
-        if let Some(link) = link {
-            // check url is valid
-            let url = link.get(2).unwrap().as_str();
-            if !url.starts_with("http") {
-                continue;
-            }
-            let res = reqwest::blocking::get(url);
-            if let Err(e) = &res {
-                println!("[{}] Error: {}", url, e);
-            } else {
-                // check if the link is valid
-                let status = res.unwrap().status();
-                if !status.is_success() {
-                    println!("[{}] is invalid", url);
+    for (index, line) in readme.iter() {
+        let head = regex::Regex::new(r"^(#*) (.*)$").unwrap();
+        if let Some(caps) = head.captures(line) {
+            if let Some(cap) = caps.get(1) {
+                let level = cap.as_str().len();
+                let title = caps.get(2).unwrap().as_str();
+                // println!("{} {}", level, title);
+                if level >= 2 {
+                    if level > 2 {
+                        t[level - 1] = title.to_string();
+                    } else {
+                        t[level - 1] = title.to_string();
+                        t[level] = String::new();
+                        t[level + 1] = String::new();
+                    }
+                    let c = t
+                        .iter()
+                        .filter(|x| x.len() > 0)
+                        .map(|x| format!("[{}](#{})", x, x.to_lowercase().replace(" ", "-")))
+                        .collect::<Vec<String>>()
+                        .join(" / ");
+                    let next_line = readme.get(index + 2).unwrap();
+                    let reg_img = regex::Regex::new(r"^!\[.*\]\((.*)\)").unwrap();
+                    let table = if reg_img.is_match(next_line.1) {
+                        format!("| {} | {} |", c, next_line.1)
+                    } else {
+                        format!("| {} |  |", c)
+                    };
+                    res.push(table);
                 }
             }
         }
     }
+
+    // <!-- Catalogs-Start --> {res} <!-- Catalogs-End -->
+    let reg = regex::RegexBuilder::new(r"<!-- Catalogs-Start -->([\s\S]*?)<!-- Catalogs-End -->")
+        .multi_line(true)
+        .build()
+        .unwrap();
+    if let Some(caps) = reg.captures(&readme_content) {
+        println!("{}", caps.get(1).unwrap().as_str());
+    }
+
+    let readme_content = reg.replace_all(
+        &readme_content,
+        format!(
+            "<!-- Catalogs-Start -->\n{}\n\n<!-- Catalogs-End -->",
+            res.join("\n")
+        )
+        .as_str(),
+    );
+
+    fs::write("./readme.md", readme_content.as_bytes()).unwrap();
 }
